58fc27a274009e6b1fb8190d1c9e187087b4c20d
Gorra.js <gorrahabla1234@gmail.com>
2024-08-21 11:03:14 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,50 +1,83 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
   const [number, setNumber] = useState(0);
   const [date, setDate] = useState({
     day: 0,
     month: 0,
-    year: 0
-  })
+    year: 0,
+  });
 
-  const handleNumberChange = (event) => setNumber(event.target.value)
+  const getNumber = () => {
+    let url = `http://numbersapi.com/${number}?json`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setNumber(data);
+      });
+  };
+
+  const getDate = () => {
+    let url = `http://numbersapi.com/${date}/date?json`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value);
+  };
 
   const handleDateChange = (event) => {
     const inputDate = event.target.value;
-    const [inputYear, inputMonth, inputDay] = inputDate.split("-")
+    const [inputYear, inputMonth, inputDay] = inputDate.split("-");
     setDate({
       day: inputDay,
       month: inputMonth,
-      year: inputYear
+      year: inputYear,
     });
-  }
+  };
 
+  const submitNumber = (e) => {
+    e.preventDefault();
+    getNumber();
+  };
+
+  const submitDate = (e) => {
+    e.preventDefault();
+    getDate();
+  };
 
   return (
     <main>
-
       <h1>Datos sobre números y fechas</h1>
 
       <section>
-        <h2>Obtener un dato sobre un número</h2>
-        <input type="number" onChange={handleNumberChange} />
-        <button>Enviar</button>
-        <p className="result-box"></p>
+        <form>
+          <input type="number" value={number} onChange={handleNumberChange} />
+          <button type="submit" onClick={submitNumber}>
+            Enviar
+          </button>
+          <h3>{number.text}</h3>
+        </form>
       </section>
 
       <section>
-        <h2>Obtener un dato sobre una fecha</h2>
-        <input type="date" onChange={handleDateChange} />
-        <button>Enviar</button>
-        <h3>Dato sobre la fecha</h3>
-        <p className="result-box"></p>
-        <h3>Dato sobre el año</h3>
-        <p className="result-box"></p>
+        <form>
+          <h2>Obtener un dato sobre una fecha</h2>
+          <input type="date" value={date} onChange={handleDateChange}/>
+          <button type="submit" onClick={submitDate}>
+            Enviar
+          </button>
+          <h3>Dato sobre la fecha</h3>
+          <p className="result-box"></p>
+          <h3>Dato sobre el año</h3>
+          <p className="result-box"></p>
+        </form>
       </section>
-
     </main>
   );
 }
